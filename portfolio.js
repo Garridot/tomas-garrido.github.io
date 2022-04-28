9e84a5f7f4ce80e13aa66ef5525256f1bb3a071a
Garridot <garridot210@gmail.com>
2022-04-28 19:45:08 -0300

    add animation

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,54 @@
+
+
+var intro   = document.querySelector('.intro')
+var main    = document.querySelector('main')
+var section = document.querySelector('.content')
+
+section.style.display = 'none'
+
+window.addEventListener('load',()=>{
+
+  var textWrapper = document.querySelector('.ml11 .letters');
+    textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+    anime.timeline({loop: false})
+    .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+    })
+    .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+    }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+    }).add({
+    targets: '.ml11 .line',
+    opacity: [1,0],
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+    });
+
+  setTimeout(animation,2000)
+  
+  function animation(){   
+    
+    section.style.display = 'block'    
+    
+  }
+})
+  
+
 var icon_menu = document.querySelector('.icon-menu')
 var menu      = document.querySelector('.menu')
 
@@ -8,33 +59,65 @@ icon_menu.addEventListener("click",function(){
 
 var nav = document.querySelector('#nav')
 
-window.addEventListener("wheel", function(event){
+window.addEventListener("wheel", function(e){  
+
+  if(e.wheelDelta >= 0){        
+      nav.style.top = "0" 
+      nav.style.transition= 'all .5s'         
+      nav.style.color = "white"      
     
-    if(event.wheelDelta >= 0){        
-        nav.style.top = "0" 
-        nav.style.transition= 'all .5s'         
-        nav.style.color = "white"
-        
-        
-        
-      
-    }else{
-        nav.style.top = "-6rem" 
-        nav.style.transition = 'all .5s'          
-                  
-      
-    }
-  })
+  }else{
+      nav.style.top = "-6rem" 
+      nav.style.transition = 'all .5s'  
+    
+  }
+})
 
-var projects  = document.querySelector('.hg')
-var project_1 = document.querySelector('._1')
-var project_2 = document.querySelector('._2')
+
+
+
+var projects  = document.querySelector('.projects')
+var project_1 = document.querySelector('#_1')
+var project_2 = document.querySelector('#_2')
+var project_3 = document.querySelector('#_3')
+var project_4 = document.querySelector('#_4')
+
+
+
+projects.onwheel = function (e) {    
+  
+  var pointY =  e.wheelDelta
+  setTransform(pointY=pointY)
+}
+
+
+
+
+function setTransform(pointY) {  
+
+  project_1.style.transform = "translateY("+ pointY +"px)"
+  if (project_1.style.transform = "translateY(0px)"){ 
+    project_2.style.transform = "translateY("+ pointY +"px)";    
+    
+  }else{
+    project_1.style.transform = "translateY("+ pointY +"px)";
+  }
+
+  if(project_2.style.transform = "translateY(120px)"){   
+    project_2.style.transform = "translateY(0px)";    
+  } 
+
+
+  if(project_3.style.transform = "translateY(120px)"){   
+    project_3.style.transform = "translateY(0px)";    
+  } 
+  if(project_4.style.transform = "translateY(120px)"){   
+    project_4.style.transform = "translateY(0px)";    
+  } 
+}      
+
 
     
-        
-
-
-
      
         
     
